18c2f5495c671ae7dcbf59134cd78562dadc5564
mingoo36 <happyalsrn1217@gmail.com>
2024-11-20 10:50:48 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -0,0 +1,8 @@
+def solution(score):
+    avg = [sum(i)/2 for i in score]
+    s_avg = sorted(avg, reverse=True)
+    
+    answer = []
+    for i in avg:
+        answer.append(s_avg.index(i)+1)
+    return answer
