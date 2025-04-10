1496316c17f8ce5931363882dd8776d10c7bdfef
mingoo36 <happyalsrn1217@gmail.com>
2025-04-10 09:45:57 +0900

    [Silver IV] Title: 제로, Time: 68 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,16 @@
+# 10773번
+
+import sys
+input = sys.stdin.readline
+
+stack = [] 
+
+for _ in range(int(input())):
+  x = int(input())
+  
+  if x ==0:
+    stack.pop()
+  else:
+    stack.append(x)
+
+print(sum(stack))
