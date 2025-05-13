af2bb50bf7ec706b954e65d347294abcfb3c3b22
mingoo36 <happyalsrn1217@gmail.com>
2025-05-13 17:57:27 +0900

    [level 2] Title: 전화번호 목록, Time: 96.11 ms, Memory: 30 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,13 @@
+# def solution(phone_book):
+#     for i in phone_book:
+#         for j in phone_book:
+#             if i!=j and i.startswith(j):
+#                 return False
+#     return True
+
+def solution(phone_book):
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
+    return True
