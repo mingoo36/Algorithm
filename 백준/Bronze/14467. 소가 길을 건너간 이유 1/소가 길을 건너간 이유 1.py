e781b9e5a14bdebccfa4982698bfccf0b7019f3e
mingoo36 <happyalsrn1217@gmail.com>
2025-05-22 23:56:10 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
@@ -0,0 +1,17 @@
+
+n = int(input())
+cow = [-1 for _ in range(11)]
+count = 0
+
+for _ in range(n):
+    a, b = map(int, input().split())
+
+    if cow[a] == -1:
+        cow[a] = b
+    elif cow[a] != b:
+        cow[a] = b
+        count +=1
+    else:
+        continue
+
+print(count)
