2a1cde47baab965f05df53fa061c8419933ff9e3
mingoo36 <happyalsrn1217@gmail.com>
2025-05-29 20:14:25 +0900

    [Silver V] Title: 거스름돈, Time: 96 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.py
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -1,11 +1,9 @@
-n = int(input())
+money = int(input())
 
-dp = [float('inf')] * (n + 1)
-dp[0] = 0  
+dp = [0] + [float('inf')] * money
 
-for i in range(1, n + 1):
-    for coin in [2, 5]:
-        if i - coin >= 0:
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+for coin in [2,5]:
+    for i in range(coin, money+1):
+        dp[i] = min(dp[i], dp[i-coin]+1)
 
-print(dp[n] if dp[n] != float('inf') else -1)
+print(dp[money] if dp[money] != float('inf') else -1)
