d014253cbf5c5dc47eea0be4f97e014cb6c6664a
mingoo36 <happyalsrn1217@gmail.com>
2025-06-07 04:15:47 +0900

    [level 2] Title: 올바른 괄호, Time: 6.51 ms, Memory: 9.46 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,18 @@
+def solution(s):
+    answer = True
+    stack = []
+    
+    for i in s:
+        if i == '(':
+            stack.append(i)
+        else:
+            if stack:
+                stack.pop()
+            else:
+                return False
+        
+    if stack:
+        return False
+            
+
+    return True
