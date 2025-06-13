9a56a8688611b70872aa11ca0be7124ebbf19d75
mingoo36 <happyalsrn1217@gmail.com>
2025-06-14 00:53:30 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 35056 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,38 +1,28 @@
 from collections import deque
 
-N, M = map(int, input().split())
+n,m = map(int,input().split())
 
-graph = []
+graph = [list(map(int, input())) for _ in range(n)]
 
-for _ in range(N):
-    graph.append(list(map(int, input())))
+result = [[False]* m for _ in range(n)]
+# 목표 지점은 (n-1 ,m-1)
 
-def BFS(x, y):
-    # 이동할 상, 하, 좌, 우 방향 정의
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
+queue = deque([(0,0)])
+result[0][0] = 1
 
-    queue = deque()
-    queue.append((x,y))
+dx = [0,0,-1,1]
+dy = [1,-1,0,0]
 
-    while queue:
-        x, y = queue.popleft()
-        # 현재 위치에서 4가지 방향 위치 확인
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            # 위치 벗어나면 안되므로 조건 추가
-            if nx<0 or nx>=N or ny<0 or ny>=M:
-                continue
-            # 벽이므로 진행 불가
-            if graph[nx][ny]==0:
-                continue
-            # 벽이 아니므로 이동 가능
-            if graph[nx][ny]==1:
-                graph[nx][ny] = graph[x][y]+1
+while queue:
+    x,y = queue.popleft()
+
+    for i in range(4):
+        nx = dx[i] + x
+        ny = dy[i] + y
+
+        if 0 <= nx < n and 0 <= ny <m:
+            if graph[nx][ny] == 1 and not result[nx][ny]:
+                result[nx][ny] = result[x][y] + 1
                 queue.append((nx,ny))
 
-    # 마지막 값에서 카운트 값 뽑기
-    return graph[N-1][M-1]
-
-print(BFS(0,0))
+print(result[n-1][m-1])
