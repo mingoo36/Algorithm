69c376261e1bb36c5a63bb3ef9346fae59856467
mingoo36 <happyalsrn1217@gmail.com>
2025-06-23 21:01:47 +0900

    [Silver III] Title: N번째 큰 수, Time: 824 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -1,6 +1,8 @@
-import heapq
+import heapq, sys
 # 파이썬에서 heapq은 기본적으로 최소 힙임, 최대힙 구현하려면 value 음수로 바꿔 넣고 나중에 다시 양수로 바꿈
 # 힙은 푸시팝가능 푸시합(힙, 벨류) -> 벨류를 넣으면서 최소값 꺼냄
+
+input = sys.stdin.readline
 heap = []
 
 n = int(input())
@@ -17,4 +19,4 @@ for _ in range(n):
             if num > heap[0]:
                 heapq.heappushpop(heap, num)
 
-print(heapq.heappop(heap))
+print(heap[0])
