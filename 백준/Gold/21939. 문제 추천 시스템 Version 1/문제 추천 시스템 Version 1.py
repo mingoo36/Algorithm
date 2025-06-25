59da95071534f5eaf9602fe88523c831620a8d84
mingoo36 <happyalsrn1217@gmail.com>
2025-06-25 19:43:54 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 228 ms, Memory: 60944 KB -BaekjoonHub

--- 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.py
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.py
@@ -1,40 +1,42 @@
-import heapq
-import sys
+import heapq, sys
 from collections import defaultdict
 
 input = sys.stdin.readline
-
-minHeap = []
-maxHeap = []
-solved = defaultdict(int)
 n = int(input())
 
+min_heap = []
+max_heap = []
+solved = defaultdict(int)
+
 for _ in range(n):
-    p, l = map(int, input().split())
-    heapq.heappush(minHeap, (l, p))
-    heapq.heappush(maxHeap, (-l, -p))
+    num , level = map(int, input().split())
+    heapq.heappush(min_heap,(level, num))
+    heapq.heappush(max_heap,(-level, -num))
 
 m = int(input())
-for _ in range(m):
-    command = input().split()
 
-    if command[0] == "recommend":
-        # 어려운 문제 추천
-        if command[1] == '1':
-            while solved[abs(maxHeap[0][1])] != 0:
-                solved[abs(maxHeap[0][1])] -= 1
-                heapq.heappop(maxHeap)
-            print(-maxHeap[0][1])
-        # 쉬운 문제 추천
+for _ in range(m):
+    x = list(input().split())
+    cmd = x[0]
+
+    if cmd == 'add':
+        num , level = int(x[1]), int(x[2])
+        heapq.heappush(min_heap,(level, num))
+        heapq.heappush(max_heap,(-level, -num))
+    
+    elif cmd == 'recommend':
+        if x[1] == '1':
+            while solved[abs(max_heap[0][1])] != 0:
+                solved[abs(max_heap[0][1])] -= 1
+                heapq.heappop(max_heap)
+            print(-max_heap[0][1])
+        
         else:
-            while solved[minHeap[0][1]] != 0:
-                solved[minHeap[0][1]] -= 1
-                heapq.heappop(minHeap)
-            print(minHeap[0][1])
-    elif command[0] == "add":
-        p = int(command[1])
-        l = int(command[2])
-        heapq.heappush(minHeap, (l, p))
-        heapq.heappush(maxHeap, (-l, -p))
-    elif command[0] == "solved":
-        solved[int(command[1])] += 1
+            while solved[abs(min_heap[0][1])] != 0:
+                solved[abs(min_heap[0][1])] -= 1
+                heapq.heappop(min_heap)
+            print(min_heap[0][1])
+
+
+    elif cmd == 'solved':
+        solved[int(x[1])] += 1
