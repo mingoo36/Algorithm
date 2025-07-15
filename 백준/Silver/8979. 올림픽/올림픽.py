8a5f9044ecdc3cacbc4fd95c9462f231dc617574
mingoo36 <happyalsrn1217@gmail.com>
2025-07-16 00:51:01 +0900

    [Silver V] Title: 올림픽, Time: 96 ms, Memory: 33432 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,22 +1,32 @@
-n, t = map(int, input().split())
-
-result = []
+n, k = map(int, input().split())
+countries = []
 
 for _ in range(n):
     country = list(map(int, input().split()))
-    result.append(country)
+    countries.append(country)
 
-result.sort(key=lambda x: (x[1], x[2], x[3]),  reverse=True)
+# 금, 은, 동 기준 내림차순 정렬
+countries.sort(key=lambda x: (-x[1], -x[2], -x[3]))
 
-rank = 1
-first = result[0][1:]
+rank = 1  # 현재 등수
+same = 1  # 공동 등수 카운터
+prev = countries[0][1:]  # 이전 메달
 
-for i in range(n):
-    if i>0:
-        if result[i][1:] != first:
-            rank += 1
-            first = result[i][1:]
-        
-    if result[i][0] == t:
-        print(rank)
-        break
+# 국가 번호: 등수 저장
+ranks = {countries[0][0]: 1}
+
+for i in range(1, n):
+    curr = countries[i][1:]
+    
+    if curr == prev:
+        # 공동 등수
+        ranks[countries[i][0]] = rank
+        same += 1
+    else:
+        # 새로운 등수
+        rank = i + 1
+        ranks[countries[i][0]] = rank
+        prev = curr
+        same = 1
+
+print(ranks[k])
