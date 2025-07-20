eb53fc6f0d23389eb2f0f04beaab10080ff90087
mingoo36 <happyalsrn1217@gmail.com>
2025-07-21 02:19:51 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -1,28 +1,35 @@
-import sys
-
-def input():
-    return sys.stdin.readline()
-
-s = int(input())
-status = [0] + list(map(int, input().split()))  # 1번 인덱스부터
+switch_len = int(input())
+switch = list(map(int, input().split()))
 
 n = int(input())
-for _ in range(n):
-    a, b = map(int, input().split())
-    if a == 1:  
-        for i in range(b, s + 1, b):
-            status[i] = 1 - status[i]
-    elif a == 2:  
-        l = r = b
-        while l > 0 and r <= s and status[l] == status[r]:
-            l -= 1
-            r += 1
-        l += 1
-        r -= 1
-        for i in range(l, r + 1):
-            status[i] = 1 - status[i]
 
-for i in range(1, s + 1):
-    print(status[i], end=' ')
-    if i % 20 == 0:
+def change(i):
+    switch[i] = 1 - switch[i]
+
+for _ in range(n):
+    sex, num = map(int, input().split())
+
+    if sex == 1:
+        for i in range(switch_len):
+            if (i+1) % num == 0:
+                change(i)
+    
+    else:
+        num -= 1  
+        change(num)
+
+        i = 1
+
+        while (num - i) >=0 and (num + i) < switch_len:
+            if switch[num - i] != switch[num + i]:
+                break
+            change(num - i)
+            change(num + i)
+            i += 1
+            
+
+
+for i in range(switch_len):
+    print(switch[i], end=' ')
+    if (i+1) % 20 == 0:
         print()
