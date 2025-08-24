1f8bcb01d54d4f4a32794c05903282a5d08f59c4
mingoo36 <happyalsrn1217@gmail.com>
2025-08-24 19:41:21 +0900

    [Silver III] Title: 1, Time: 140 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.py
@@ -0,0 +1,14 @@
+while True:
+    try:
+        n = int(input())
+    except:
+        break
+    
+    num = 1
+    cnt = 1
+    
+    while num % n != 0:
+        num = num * 10 + 1
+        cnt += 1
+    
+    print(cnt)
