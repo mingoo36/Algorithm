32cdf1c2c0fdfbad62562eb49e34a8ea516b988a
mingoo36 <happyalsrn1217@gmail.com>
2025-08-31 01:13:31 +0900

    [Silver I] Title: 숨바꼭질, Time: 88 ms, Memory: 36972 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -3,25 +3,24 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
+Max = 100001
+dist = [0] * Max
 
 def dfs(n,k):
     if n >= k:
         return n - k
     
-    Max = 100000
-    visited = [False] * (Max + 1)
-    visited[n] = True
-    queue = deque([(n,0)])
+    queue = deque([n])
 
     while queue:
-        x, t = queue.popleft()
+        x = queue.popleft()
 
         if x == k:
-            return t
+            return dist[x]
         
         for nx in (x-1, x+1, x*2):
-            if 0 <= nx <= Max and not visited[nx]:
-                visited[nx] = True
-                queue.append((nx, t+1))
+            if 0 <= nx < Max and not dist[nx]:
+                dist[nx] = dist[x] + 1
+                queue.append(nx)
         
 print(dfs(n,k))
